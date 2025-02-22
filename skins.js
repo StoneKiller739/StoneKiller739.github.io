38b53e4d364c57c72051ad69ff194965fcaa50f0
StoneKiller739 <fahadatawi42@gmail.com>
2025-02-22 12:29:38 -0800

    Add files via upload

---
+++ skins.js
@@ -0,0 +1,10 @@
+let skinUrls = [
+    'https://imgur.com/a/bxM4kJD',
+    'https://imgur.com/a/aWSscl3',
+    'https://imgur.com/a/YBy1aTt',
+    'https://imgur.com/a/KG9HF7o'
+    'https://imgur.com/a/Oom1ELt'
+    'https://imgur.com/a/pkP0bCe'
+    // Add more URLs as needed
+];
+
